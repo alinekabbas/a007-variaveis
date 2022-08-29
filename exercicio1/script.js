6f602e9705148a57918a0140959fa406ce1c8777
alinekabbas <alinekabbas@gmail.com>
2022-08-29 14:56:06 -0300

    exercicios 1 e 2 resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Qual é o seu nome?")
+let idade = Number (prompt("Qual é a sua idade?"))
+console.log(typeof nome)
+console.log(idade, typeof idade)
+console.log("Olá " ,nome," você tem " ,idade," anos.")
+
+//Resposta: undefined. Não foi definido o tipo de dados das variáveis.
+//Resposta: imprimiu os tipos de dados das variáveis
+
